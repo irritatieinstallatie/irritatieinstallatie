ae3636f1de326510b5bef80d680eec6dfcc40366
gerben kolkena <68428746+irritatieinstallatie@users.noreply.github.com>
2025-09-09 08:20:00 +0200

    Add files via upload

---
+++ GK2025_08_24_2.rb
@@ -0,0 +1,55 @@
+set_volume! 1
+xioja = rrand_i(7,11)
+live_loop :GK2025_08_24_1A do
+  use_bpm 100
+  with_synth :bass_foundation do
+    with_fx :pitch_shift, time_dis: 0.125, window_size: 0.75 do
+      with_fx :gverb, spread: 0.5, room: 109 do
+        with_fx :bitcrusher, bits: (ring 2,3,4,5).reflect.tick, cutoff: (ring 60, 70, 80, 90, 100).reflect.tick do
+          xioja.times do
+            play 20,
+              attack: 0.001,
+              sustain: 0.25,
+              release: 0.75
+            play 22,
+              attack: 0.001,
+              sustain: 0.25,
+              release: 0.75
+            play 30,
+              attack: 0.001,
+              sustain: 0.25,
+              release: 0.75
+            sleep (ring 0.5, 0.25).choose
+          end
+        end
+      end
+    end
+  end
+end
+live_loop :GK2025_08_24_1B do
+  use_bpm 100
+  use_random_seed (ring 109, 110, 111).tick
+  with_synth :gabberkick do
+    with_fx :compressor, relax_time: (ring 0.125, 0.25, 0.5, 0.75).choose do
+      with_fx :gverb, damp: 0.75, dry: 0.75, spread: 0.875, room: 65 do
+        with_fx :bitcrusher, bits: 4, cutoff: rrand_i(80,88) do
+          xioja.times do
+            play 20,
+              attack: 0.001,
+              sustain: 0.125,
+              release: 0.125
+            play 22,
+              attack: 0.001,
+              sustain: 0.125,
+              release: 0.0125
+            play 40,
+              attack: 0.001,
+              sustain: 0.125,
+              release: 0.0125
+            sleep (ring 0.5, 0.25).choose
+          end
+        end
+      end
+    end
+  end
+end
