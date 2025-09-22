e259800a1d1c48059ca9b8f1726e57c85339ad46
gerben kolkena <68428746+irritatieinstallatie@users.noreply.github.com>
2025-09-22 14:17:50 +0200

    Add files via upload

---
+++ 20230425_arduino_mic_tvout_08_FINAL_RAN.ino
@@ -0,0 +1,21 @@
+#include <TVout.h>
+TVout TV;
+int pot = 128; 
+void setup() {
+  TV.begin(PAL, 128, 96);
+}
+void loop() {
+  int mic = analogRead(A0);
+//  int pot = analogRead(A1);
+  for (int i = 0; i < 1; i++) { 
+    int x = random(127);
+    int y = random(95);
+    if (mic >= random(112,128)) { 
+      TV.draw_rect(x, y, 1, 1, WHITE);
+    } else { 
+      TV.draw_rect(x, y, 1, 1, BLACK);
+      TV.clear_screen();
+    }
+  }
+// TV.delay_frame(0); 
+}
