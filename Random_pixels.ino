9eedf8a6c8a2372257317daa6a0b5052a5a3b02e
gerben kolkena <68428746+irritatieinstallatie@users.noreply.github.com>
2025-09-22 11:18:33 +0200

    Add files via upload

---
+++ Random_pixels.ino
@@ -0,0 +1,22 @@
+﻿#include <TVout.h>
+TVout TV;
+
+void setup() {
+  TV.begin(PAL, 128, 96);
+}
+
+void loop() {
+  int mic = analogRead(A0);
+  
+  for (int i = 0; i < 1; i++) {
+    int x = random(127);
+    int y = random(95);
+
+    if (mic >= random(112, 128)) {
+      TV.draw_rect(x, y, 1, 1, WHITE);
+    } else {
+      TV.draw_rect(x, y, 1, 1, BLACK);
+      TV.clear_screen();
+    }
+  }
+}
